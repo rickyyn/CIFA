9648dbd9ca14000f4df29cc014aaca92bf16d4f8
Marcos Martins <mrsaocwork@gmail.com>
2026-04-12 19:02:36 -0300

    refactor: cifa-desktop next

--- CIFA-Front-end/src/app/components/cadastrar-aluno/cadastrar-aluno.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cadastrar-aluno',
-  imports: [],
-  templateUrl: './cadastrar-aluno.html',
-  styleUrl: './cadastrar-aluno.css',
-})
-export class CadastrarAluno {}
