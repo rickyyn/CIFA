3ab07a43b402091890579780ce60b1f77c80142e
Fabio Lima <fabiojosedelima011@gmail.com>
2026-04-12 23:56:21 -0300

    add funcao qrcode

--- API/src/main/java/br/edu/fatecpg/cifa/controller/AlunoController.java
+++ API/src/main/java/br/edu/fatecpg/cifa/controller/AlunoController.java
@@ -1,19 +1,20 @@
 package br.edu.fatecpg.cifa.controller;
 
+import br.edu.fatecpg.cifa.service.AlunoService;
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.Map;
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
     @PostMapping("/criarAluno")
     public String criarAluno(@RequestBody Map<String, Object> dados) throws Exception {
         Firestore db = FirestoreClient.getFirestore();
@@ -23,7 +24,15 @@ public class AlunoController {
         aluno.put("createdAt", Timestamp.now());
         DocumentReference docRef = db.collection("Alunos").document();
         docRef.set(aluno);
-
         return docRef.getId();
     }
+
+    @Autowired
+    private AlunoService alunoService;
+
+    @PostMapping("/ler-qr")
+    public String lerQr(@RequestParam("file") MultipartFile file) {
+        return alunoService.verificarExpiracao(file);
+    }
+
 }
