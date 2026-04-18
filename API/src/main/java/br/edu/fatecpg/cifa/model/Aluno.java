031b97f79056081346131f5f48688abeb48fcab3
Fabio Lima <fabiojosedelima011@gmail.com>
2026-04-18 18:51:24 -0300

    Endpoints do Aluno

---
+++ API/src/main/java/br/edu/fatecpg/cifa/model/Aluno.java
@@ -0,0 +1,28 @@
+package br.edu.fatecpg.cifa.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.annotation.DocumentId;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Aluno {
+
+    @DocumentId
+    private String id;
+
+    private String nome;
+    private String email;
+    private Long ra;
+    private String id_curso;
+    private String rfid_tag;
+    private boolean status_ativo;
+    private boolean esta_no_campus;
+    @JsonIgnore
+    private Timestamp createdat;
+    @JsonIgnore
+    private Timestamp updatedat;
+}
