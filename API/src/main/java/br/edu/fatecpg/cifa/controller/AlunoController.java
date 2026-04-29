929a1cb8a68648433be6b0932bf6507efa4150cf
rickyyn <ritsccd@gmail.com>
2026-04-29 20:27:07 -0300

    Espero que tenha resolvido o problema

--- API/src/main/java/br/edu/fatecpg/cifa/controller/AlunoController.java
+++ API/src/main/java/br/edu/fatecpg/cifa/controller/AlunoController.java
@@ -1,29 +1,74 @@
 package br.edu.fatecpg.cifa.controller;
 
+import br.edu.fatecpg.cifa.dto.AlunoDto;
+import br.edu.fatecpg.cifa.model.Aluno;
+import br.edu.fatecpg.cifa.service.AlunoService;
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
-    @PostMapping("/criarAluno")
-    public String criarAluno(@RequestBody Map<String, Object> dados) throws Exception {
-        Firestore db = FirestoreClient.getFirestore();
-        Map<String, Object> aluno = new HashMap<>();
-        aluno.put("nome", dados.get("nome"));
-        aluno.put("ra", dados.get("ra"));
-        aluno.put("createdAt", Timestamp.now());
-        DocumentReference docRef = db.collection("Alunos").document();
-        docRef.set(aluno);
 
-        return docRef.getId();
+    @Autowired
+    private AlunoService alunoService;
+
+    @PostMapping("/ler-qr")
+    public String lerQr(@RequestParam("file") MultipartFile file) {
+        return alunoService.verificarExpiracao(file);
     }
+
+    @PostMapping(value = "/adicionarAluno", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> adicionar(@ModelAttribute Aluno aluno, @RequestParam("foto") MultipartFile foto) {
+        String id = alunoService.cadastrarAluno(aluno, foto);
+        return ResponseEntity.ok("Aluno criado com ID: " + id);
+    }
+
+    @GetMapping("/verAlunos")
+    public ResponseEntity<List<Aluno>> exibirAlunos() {
+        List<Aluno> lista = alunoService.exibirAlunos();
+        return ResponseEntity.ok(lista);
+    }
+
+    @PutMapping("/editarAluno/{id}")
+    public ResponseEntity<String> editar(@PathVariable String id, @RequestBody Aluno aluno){
+        alunoService.editarAluno(id, aluno);
+        return ResponseEntity.ok("Aluno atualizado");
+    }
+
+    @DeleteMapping("/excluirAluno/{id}")
+    public ResponseEntity<String> excluir(@PathVariable String id){
+        alunoService.excluirAluno(id);
+        return ResponseEntity.ok("Aluno excluido");
+    }
+
+    @PostMapping("/verificarAluno")
+    public ResponseEntity<String> verificarAluno(@RequestBody Map<String, Object> payload) {
+
+        String nome = (String) payload.get("nome");
+
+        Long ra = ((Number) payload.get("ra")).longValue();
+
+        String dataExpiracao = (String) payload.get("expira_em");
+
+        return ResponseEntity.ok(alunoService.verificarAlunoBd(ra, nome, dataExpiracao));
+    }
+
+    @GetMapping("/encontrarPorId/{id}")
+    public ResponseEntity<Aluno> exibirAlunoPorId(@PathVariable String id){
+        Aluno aluno = alunoService.encontrarPorId(id);
+        return ResponseEntity.ok(aluno);
+    }
+
+
 }
--- API/src/main/java/br/edu/fatecpg/cifa/model/Aluno.java
+++ API/src/main/java/br/edu/fatecpg/cifa/model/Aluno.java
@@ -12,12 +12,18 @@ import lombok.NoArgsConstructor;
 public class Aluno {
 
     @DocumentId
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String id;
     private String nome;
+    private String email_pessoal;
+    private String email_institucional;
     private Long ra;
+    private String id_turma;
     private String rfid_tag;
     private boolean status_ativo;
     private boolean esta_no_campus;
+    private String imagem_url;
+    private int ciclo_atual;
     @JsonIgnore
     private Timestamp createdat;
     @JsonIgnore
